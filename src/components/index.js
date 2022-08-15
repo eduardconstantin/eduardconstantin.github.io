54724d8e1c7ff73113a4bae2efdd69dbc0a7cf79
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2022-08-15 19:46:10 +0300

    Export components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Menu } from './Menu/Menu';
+export { default as Welcome } from './Welcome/Welcome';
+export { default as About } from './About/About';
+export { default as Skills } from './Skills/Skills';
+export { default as Projects } from './Projects/Projects';
