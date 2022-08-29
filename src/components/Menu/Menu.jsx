b5f706290b3fa2bf4881f8f8bbac89b520e09994
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2022-08-29 20:00:48 +0300

    Fix hover issue on social icons

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -8,19 +8,34 @@ import { menuWrapperAnim, buttonIconAnim, menuButtonAnim, menuAnim } from './Men
 
 export default function Menu({ currentPage, selectPage }) {
 	const [hoverPage, setHoverPage] = useState(currentPage);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	return (
 		<motion.section className='menuSection' variants={menuAnim} initial='init' animate='anim'>
 			<Location />
 
-			<motion.div className='menuContainer' initial='close' animate='close' whileHover='open'>
-				<motion.div className='menuButton' variants={menuButtonAnim}>
+			<motion.div className='menuContainer'>
+				<motion.div
+					className='menuButton'
+					variants={menuButtonAnim}
+					initial='close'
+					animate={isMenuOpen ? 'open' : 'close'}
+					onHoverStart={() => setIsMenuOpen(true)}
+					onHoverEnd={() => setIsMenuOpen(false)}
+				>
 					<motion.div className='top' variants={buttonIconAnim} custom={1}></motion.div>
 					<motion.div className='left' variants={buttonIconAnim} custom={2}></motion.div>
 					<motion.div className='right' variants={buttonIconAnim} custom={3}></motion.div>
 					<motion.div className='bottom' variants={buttonIconAnim} custom={4}></motion.div>
 				</motion.div>
-				<motion.div variants={menuWrapperAnim} className='menuWrapper'>
+				<motion.div
+					variants={menuWrapperAnim}
+					className='menuWrapper'
+					initial='close'
+					animate={isMenuOpen ? 'open' : 'close'}
+					onHoverStart={() => setIsMenuOpen(true)}
+					onHoverEnd={() => setIsMenuOpen(false)}
+				>
 					<div className='menu'>
 						<ul className='menuList'>
 							{MENU_LINKS.map(({ name, link }, i) => {
