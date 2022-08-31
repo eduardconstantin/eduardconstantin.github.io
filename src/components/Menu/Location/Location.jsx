c4c74e0323c504d7b264a630e8750f861fabf826
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2022-08-31 19:26:08 +0300

    Add class names to location

--- src/components/Menu/Location/Location.jsx
+++ src/components/Menu/Location/Location.jsx
@@ -5,9 +5,9 @@ import { mapAnim } from './Location.anim';
 export const Location = () => {
 	return (
 		<div className='locationContainer'>
-			<motion.p inital='init' animate='init' whileHover='hover'>
+			<motion.p className='location' inital='init' animate='init' whileHover='hover'>
 				Located in <span>Romania</span>
-				<motion.span className='map' variants={mapAnim}></motion.span>
+				<motion.span className='locationMap' variants={mapAnim}></motion.span>
 			</motion.p>
 		</div>
 	);
