2ce9959d52610061e678cb168f10880e820a96ed
Eduard Constantin <ibinceanu.eduard@yahoo.com>
2022-09-04 15:32:45 +0300

    Console log tracking id

--- src/helpers/useGaTracker.js
+++ src/helpers/useGaTracker.js
@@ -5,7 +5,7 @@ export const useGaTracker = (location) => {
 	const TRACKING_ID = process.env.REACT_APP_TRACKING_ID;
 
 	ReactGA.initialize(TRACKING_ID);
-	console.log(location.pathname);
+	console.log(TRACKING_ID);
 	useEffect(() => {
 		ReactGA.pageview(location.pathname + location.search);
 	}, [location]);
