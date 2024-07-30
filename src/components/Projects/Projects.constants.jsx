1747127430e3faab8f7c461cb75ecbcf2bb3eec1
Eduard-Constantin Ibinceanu <ibinceanu.eduard@yahoo.com>
2024-07-30 16:22:41 +0000

    Add projects to the app

--- src/components/Projects/Projects.constants.jsx
+++ src/components/Projects/Projects.constants.jsx
@@ -3,13 +3,19 @@ import dribbbleLink from '../../assets/images/links/dribbble.svg';
 import gitLink from '../../assets/images/links/github.svg';
 import ytLink from '../../assets/images/links/youtube.svg';
 
-import jdsoftware from '../../assets/images/projects/jdsoftware.png';
 import portfolio from '../../assets/images/projects/portfolio.png';
 import livrator from '../../assets/images/projects/livrator.png';
 import ghicicuvantul from '../../assets/images/projects/gc.png';
 import cryptocurrency from '../../assets/images/projects/cc.png';
 import traveldeck from '../../assets/images/projects/td.png';
 import xboxdash from '../../assets/images/projects/xd.png';
+import framerbtn from "../../assets/images/projects/framerbtn.png";
+import doticons from "../../assets/images/projects/doticons.png";
+import cs2btn from "../../assets/images/projects/cs2btn.png";
+import exploreasy from "../../assets/images/projects/exploreasy.png";
+import storybookgenie from "../../assets/images/projects/storybookgenie.png";
+import azurecloudexams from "../../assets/images/projects/azurecloudexams.png";
+import habithero from "../../assets/images/projects/habithero.png";
 
 import geex from '../../assets/images/projects/geex.png';
 import tla from '../../assets/images/projects/tla.png';
@@ -32,163 +38,235 @@ import vibenow from '../../assets/images/projects/vibenow.png';
 import dynamicmenu from '../../assets/images/projects/dynamicmenu.png';
 
 export const web = [
-	{
-		name: 'Portfolio',
-		date: '2022-05-08',
-		cover: portfolio,
-		links: [{ icon: gitLink, link: 'https://github.com/eduardconstantin/eduardconstantin.github.io' }],
-	},
-	{
-		name: 'Livrator.ro',
-		date: '2022-04-30',
-		cover: livrator,
-		links: [{ icon: webLink, link: 'https://www.livrator.ro' }],
-	},
-	{ name: 'Ghiceste cuvantul',
-		date: '2022-03-20',
-		cover: ghicicuvantul,
-		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/19659643-Ghiceste-cuvantul-Guess-the-word' }] },
-	{
-		name: 'Xbox Dashboard',
-		date: '2022-03-06',
-		cover: xboxdash,
-		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/17656116-Xbox-Dashboard-Animation' }],
-	},
-	{
-		name: 'Crypto Currencies',
-		date: '2021-10-21',
-		cover: cryptocurrency,
-		links: [
-			{ icon: gitLink, link: 'https://github.com/eduardconstantin/Top-three-crypto' },
-			{ icon: dribbbleLink, link: 'https://dribbble.com/shots/16701637-Top-three-cryptocurrencies' },
-		],
-	},
-	{
-		name: 'Travel Deck',
-		date: '2021-11-16',
-		cover: traveldeck,
-		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/16874996-Travel-Deck' }],
-	},
-	{
-		name: 'Navigation Menu Animation',
-		date: '2022-03-20',
-		cover: navui,
-		links: [
-			{ icon: gitLink, link: 'https://github.com/eduardconstantin/Navigation-menu-animation' },
-			{ icon: dribbbleLink, link: 'https://dribbble.com/shots/17766382-Navigation-menu-Animation-concept' },
-		],
-	},
-	{
-		name: 'Button Animation',
-		date: '2022-05-01',
-		cover: btnanim,
-		links: [
-			{ icon: gitLink, link: 'https://github.com/eduardconstantin/Comic-book-button-anim' },
-			{ icon: dribbbleLink, link: 'https://dribbble.com/shots/18145123-Button-Animation' },
-		],
-	},
-	{
-		name: 'JD Software',
-		date: '2022-08-14',
-		cover: jdsoftware,
-		links: [{ icon: webLink, link: 'http://www.jdsoftware.ro' }],
-	},
-	{
-		name: 'Dynamic Menu',
-		date: '2022-09-18',
-		cover: dynamicmenu,
-		links: [
-			{ icon: gitLink, link: 'https://github.com/eduardconstantin/dynamic-menu' },
-			{ icon: dribbbleLink, link: 'https://dribbble.com/shots/19413730-Dynamic-Menu' }
-		],
-	},
+  {
+    name: "Portfolio",
+    date: "2022-05-08",
+    cover: portfolio,
+    links: [{ icon: gitLink, link: "https://github.com/eduardconstantin/eduardconstantin.github.io" }],
+  },
+  {
+    name: "Livrator.ro",
+    date: "2022-04-30",
+    cover: livrator,
+    links: [{ icon: webLink, link: "https://www.livrator.ro" }],
+  },
+  {
+    name: "Xbox Dashboard",
+    date: "2022-03-06",
+    cover: xboxdash,
+    links: [{ icon: dribbbleLink, link: "https://dribbble.com/shots/17656116-Xbox-Dashboard-Animation" }],
+  },
+  {
+    name: "Crypto Currencies",
+    date: "2021-10-21",
+    cover: cryptocurrency,
+    links: [
+      { icon: gitLink, link: "https://github.com/eduardconstantin/Top-three-crypto" },
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/16701637-Top-three-cryptocurrencies" },
+    ],
+  },
+  {
+    name: "Travel Deck",
+    date: "2021-11-16",
+    cover: traveldeck,
+    links: [{ icon: dribbbleLink, link: "https://dribbble.com/shots/16874996-Travel-Deck" }],
+  },
+  {
+    name: "Navigation Menu Animation",
+    date: "2022-03-20",
+    cover: navui,
+    links: [
+      { icon: gitLink, link: "https://github.com/eduardconstantin/react-motion-components" },
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/17766382-Navigation-menu-Animation-concept" },
+    ],
+  },
+  {
+    name: "Button Animation",
+    date: "2022-05-01",
+    cover: btnanim,
+    links: [
+      { icon: gitLink, link: "https://github.com/eduardconstantin/react-motion-components" },
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/18145123-Button-Animation" },
+    ],
+  },
+  {
+    name: "Dynamic Menu",
+    date: "2022-09-18",
+    cover: dynamicmenu,
+    links: [
+      { icon: gitLink, link: "https://github.com/eduardconstantin/react-motion-components" },
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/19413730-Dynamic-Menu" },
+    ],
+  },
+  {
+    name: "Framer AI Button",
+    date: "2023-03-26",
+    cover: framerbtn,
+    links: [
+      { icon: gitLink, link: "https://github.com/eduardconstantin/react-motion-components" },
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/21022811--Framer-AI-Button" },
+    ],
+  },
+  {
+    name: "Doticons",
+    date: "2023-07-17",
+    cover: doticons,
+    links: [
+      { icon: gitLink, link: "https://github.com/eduardconstantin/doticons" },
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/22023265-Doticons-A-collection-of-minimal-dot-icons" },
+    ],
+  },
+  {
+    name: "Counter-Strike 2 Button",
+    date: "2023-09-11",
+    cover: cs2btn,
+    links: [
+      { icon: gitLink, link: "https://github.com/eduardconstantin/react-motion-components" },
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/22517905-Counter-Strike-2-Button" },
+    ],
+  },
+  {
+    name: "ExplorEasy",
+    date: "2023-11-28",
+    cover: exploreasy,
+    links: [
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/23144020-ExplorEasy-Generative-travel-itineraries" },
+    ],
+  },
+  {
+    name: "ExplorEasy",
+    date: "2023-11-28",
+    cover: exploreasy,
+    links: [
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/23144020-ExplorEasy-Generative-travel-itineraries" },
+    ],
+  },
+  {
+    name: "Storybook Genie",
+    date: "2024-03-05",
+    cover: storybookgenie,
+    links: [{ icon: gitLink, link: "https://github.com/eduardconstantin/storybook-genie" }],
+  },
+  {
+    name: "Azure Cloud Exams",
+    date: "2024-04-15",
+    cover: azurecloudexams,
+    links: [
+      { icon: gitLink, link: "https://github.com/eduardconstantin/azure-cloud-exams" },
+      {
+        icon: dribbbleLink,
+        link: "https://dribbble.com/shots/23997521-Azure-Cloud-Exams-A-Community-Driven-Open-Source-Project",
+      },
+    ],
+  },
+  {
+    name: "Habit Hero",
+    date: "2024-07-29",
+    cover: habithero,
+    links: [
+      {
+        icon: dribbbleLink,
+        link: "https://dribbble.com/shots/24592614-Habit-Hero-Generative-scheduling-web-app",
+      },
+    ],
+  },
 ];
 
 export const design = [
-	{
-		name: 'Smart Home Dashboard UI',
-		date: '2022-04-03',
-		cover: smarthome,
-		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/17912071-Smart-Home-Dashboard-UI' }],
-	},
-	{
-		name: 'God of War Concept UI',
-		date: '2022-03-13',
-		cover: godofwar,
-		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/17710279-God-of-War-Website-design' }],
-	},
-	{
-		name: 'In time Logo',
-		date: '2020-04-20',
-		cover: itlogo,
-		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/16788202-IN-TIME-Logo-design' }],
-	},
-	{
-		name: 'NY Open Offices Logo',
-		date: '2018-05-12',
-		cover: nyoologo,
-		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/18038998-New-York-Open-Offices-Logo-design' }],
-	},
-	{
-		name: 'Midnight Pulse Logo',
-		date: '2018-07-17',
-		cover: mplogo,
-		links: [{ icon: dribbbleLink, link: 'https://dribbble.com/shots/18302655-Midnight-Pulse-Logo-design' }],
-	},
-	{
-		name: 'Discover Romania Concept UI',
-		date: '2022-06-05',
-		cover: discoverro,
-		links: [
-			{ icon: dribbbleLink, link: 'https://dribbble.com/shots/18413927-Discover-Romania-Landing-page' },
-			{ icon: ytLink, link: 'https://www.youtube.com/watch?v=vzyxaw_ryyQ' },
-		],
-	},
-	{
-		name: 'Vice City Conceput UI',
-		date: '2022-08-14',
-		cover: vclp,
-		links: [
-			{ icon: dribbbleLink, link: 'https://dribbble.com/shots/19100552-Welcome-back-to-Vice-City-Landing-page' },
-			{ icon: ytLink, link: 'https://www.youtube.com/watch?v=ysBkWUQMLHQ' },
-		],
-	},
-	{
-		name: 'Vibe Now UI',
-		date: '2022-10-28',
-		cover: vibenow,
-		links: [
-			{ icon: dribbbleLink, link: 'https://dribbble.com/shots/20106594-Vibe-Now-Mobile-app-for-events' },
-		],
-	},
+  {
+    name: "Smart Home Dashboard UI",
+    date: "2022-04-03",
+    cover: smarthome,
+    links: [{ icon: dribbbleLink, link: "https://dribbble.com/shots/17912071-Smart-Home-Dashboard-UI" }],
+  },
+  {
+    name: "God of War Concept UI",
+    date: "2022-03-13",
+    cover: godofwar,
+    links: [{ icon: dribbbleLink, link: "https://dribbble.com/shots/17710279-God-of-War-Website-design" }],
+  },
+  {
+    name: "In time Logo",
+    date: "2020-04-20",
+    cover: itlogo,
+    links: [{ icon: dribbbleLink, link: "https://dribbble.com/shots/16788202-IN-TIME-Logo-design" }],
+  },
+  {
+    name: "NY Open Offices Logo",
+    date: "2018-05-12",
+    cover: nyoologo,
+    links: [{ icon: dribbbleLink, link: "https://dribbble.com/shots/18038998-New-York-Open-Offices-Logo-design" }],
+  },
+  {
+    name: "Midnight Pulse Logo",
+    date: "2018-07-17",
+    cover: mplogo,
+    links: [{ icon: dribbbleLink, link: "https://dribbble.com/shots/18302655-Midnight-Pulse-Logo-design" }],
+  },
+  {
+    name: "Discover Romania Concept UI",
+    date: "2022-06-05",
+    cover: discoverro,
+    links: [
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/18413927-Discover-Romania-Landing-page" },
+      { icon: ytLink, link: "https://www.youtube.com/watch?v=vzyxaw_ryyQ" },
+    ],
+  },
+  {
+    name: "Vice City Conceput UI",
+    date: "2022-08-14",
+    cover: vclp,
+    links: [
+      { icon: dribbbleLink, link: "https://dribbble.com/shots/19100552-Welcome-back-to-Vice-City-Landing-page" },
+      { icon: ytLink, link: "https://www.youtube.com/watch?v=ysBkWUQMLHQ" },
+    ],
+  },
+  {
+    name: "Vibe Now UI",
+    date: "2022-10-28",
+    cover: vibenow,
+    links: [{ icon: dribbbleLink, link: "https://dribbble.com/shots/20106594-Vibe-Now-Mobile-app-for-events" }],
+  },
+  {
+    name: "Storybook Genie",
+    date: "2024-03-05",
+    cover: storybookgenie,
+    links: [{ icon: dribbbleLink, link: "https://dribbble.com/shots/23770977-Storybook-Genie-Generative-CLI-tool" }],
+  },
 ];
 
 export const game = [
-	{ name: 'Ge Ex', date: '2014-09-01', cover: geex, links: [] },
-	{
-		name: 'The Last Ancient',
-		date: '2017-05-01',
-		cover: tla,
-		links: [{ icon: webLink, link: 'https://halfroundgames.itch.io/the-last-ancient' }],
-	},
-	{ name: "Bachelor's degree AR project", date: '2018-07-20', cover: arbachelor, links: [] },
-	{ name: "Master's degree AR project", date: '2020-07-20', cover: ardissertation, links: [] },
-	{
-		name: 'LG Rollable TV AR project',
-		date: '2019-01-23',
-		cover: arlgtv,
-		links: [{ icon: ytLink, link: 'https://www.youtube.com/watch?v=bn69s8lOCCU&t=26s' }],
-	},
-	{
-		name: 'Oneplus 7 AR project',
-		date: '2019-05-21',
-		cover: arop7,
-		links: [{ icon: ytLink, link: 'https://www.youtube.com/watch?v=188qht5Sg8k' }],
-	},
-	{
-		name: 'Castle Escape',
-		date: '2018-12-20',
-		cover: castleescape,
-		links: [{ icon: ytLink, link: 'https://www.youtube.com/watch?v=u_s-hL78goQ' }],
-	},
+  { name: "Ge Ex", date: "2014-09-01", cover: geex, links: [] },
+  {
+    name: "The Last Ancient",
+    date: "2017-05-01",
+    cover: tla,
+    links: [{ icon: webLink, link: "https://halfroundgames.itch.io/the-last-ancient" }],
+  },
+  { name: "Bachelor's degree AR project", date: "2018-07-20", cover: arbachelor, links: [] },
+  { name: "Master's degree AR project", date: "2020-07-20", cover: ardissertation, links: [] },
+  {
+    name: "LG Rollable TV AR project",
+    date: "2019-01-23",
+    cover: arlgtv,
+    links: [{ icon: ytLink, link: "https://www.youtube.com/watch?v=bn69s8lOCCU&t=26s" }],
+  },
+  {
+    name: "Oneplus 7 AR project",
+    date: "2019-05-21",
+    cover: arop7,
+    links: [{ icon: ytLink, link: "https://www.youtube.com/watch?v=188qht5Sg8k" }],
+  },
+  {
+    name: "Castle Escape",
+    date: "2018-12-20",
+    cover: castleescape,
+    links: [{ icon: ytLink, link: "https://www.youtube.com/watch?v=u_s-hL78goQ" }],
+  },
+  {
+    name: "Ghiceste cuvantul",
+    date: "2022-03-20",
+    cover: ghicicuvantul,
+    links: [{ icon: dribbbleLink, link: "https://dribbble.com/shots/19659643-Ghiceste-cuvantul-Guess-the-word" }],
+  },
 ];
